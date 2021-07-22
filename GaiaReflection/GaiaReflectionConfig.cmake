0d4d89fca70c39273b1db839ddc07fad9cb481d9
Vincent <rumble.vecent@outlook.com>
2021-07-22 20:53:07 +0800

    Change install scripts, and add version support.

--- GaiaReflection/GaiaReflectionConfig.cmake
+++
@@ -1,8 +0,0 @@
-cmake_minimum_required(3.10)
-
-find_path(GaiaReflection_INCLUDE_DIRS "GaiaReflection")
-find_library(GaiaReflection_LIBS "GaiaReflection")
-
-if (GaiaReflection_INCLUDE_DIRS AND GaiaReflection_LIBS)
-    set(GaiaReflection_FOUND TRUE)
-endif()
